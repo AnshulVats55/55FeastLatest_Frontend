86325300097f1cea17e00193eca5c17c54a54775
AnshulVats55 <anshul.vats@fiftyfivetech.io>
2023-11-22 10:30:46 +0530

    base url updated

--- src/api/baseUrl/BaseUrl.js
+++ src/api/baseUrl/BaseUrl.js
@@ -1,6 +1,7 @@
-const BASE_URL = "https://nice-pear-bee-sock.cyclic.app";
+const BASE_URL = "https://yellow-abalone-veil.cyclic.app";
 //1. https://light-jay-leg-warmers.cyclic.cloud
 //2. https://ivory-panda-garb.cyclic.cloud
 //3. https://brainy-blue-shoe.cyclic.app
+//4. https://nice-pear-bee-sock.cyclic.app
 
 export default BASE_URL;
